1d96e22fa21c3819f43682fca025b620cbe6bb06
Kreverson <kreversonjulho@gmail.com>
2025-03-22 20:46:29 -0300

    nest g co tecnologia --no-spec --flat

--- backend/src/tecnologia/tecnologia.module.ts
+++ backend/src/tecnologia/tecnologia.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { TecnologiaController } from './tecnologia.controller';
 
-@Module({})
+@Module({
+  controllers: [TecnologiaController]
+})
 export class TecnologiaModule {}
