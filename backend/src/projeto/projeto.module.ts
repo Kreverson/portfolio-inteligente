d5532c31c4a7d5c552da02d50db8a7dca34a19c9
Kreverson <kreversonjulho@gmail.com>
2025-03-22 21:13:23 -0300

    nest g co projeto --no-spec --flat

--- backend/src/projeto/projeto.module.ts
+++ backend/src/projeto/projeto.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProjetoProvider } from './projeto.provider';
+import { ProjetoController } from './projeto.controller';
 
 @Module({
-  providers: [ProjetoProvider]
+  providers: [ProjetoProvider],
+  controllers: [ProjetoController]
 })
 export class ProjetoModule {}
